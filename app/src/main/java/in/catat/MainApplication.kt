346dc8dfa82321457bbb32934a6e334d0713ccb2
Irfan Pertadima <pertadimairfan@gmail.com>
2020-08-22 19:39:10 +0700

    [irfan] create splash screen activity

---
+++ app/src/main/java/in/catat/MainApplication.kt
@@ -0,0 +1,13 @@
+package `in`.catat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by pertadima on 22,August,2020
+ */
+
+@HiltAndroidApp
+class MainApplication : Application() {
+
+}
---
+++ app/src/main/java/in/catat/presentation/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package `in`.catat.presentation.splashscreen
+
+import `in`.catat.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity(R.layout.activity_splash_screen) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
