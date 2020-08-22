6be22084e7fb29b337299658cb2d9d5f84c70d80
Irfan Pertadima <pertadimairfan@gmail.com>
2020-08-23 03:29:26 +0700

    [irfan] create home greetings

--- app/src/main/java/in/catat/presentation/MainActivity.kt
+++ app/src/main/java/in/catat/presentation/MainActivity.kt
@@ -1,12 +1,38 @@
 package `in`.catat.presentation
 
 import `in`.catat.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupView()
+    }
+
+    private fun setupView() {
+        home_textview_greeting.text = getString(R.string.general_text_hallo, showWelcomeMessage())
+    }
+
+    private fun showWelcomeMessage(): String {
+        val calendar = Calendar.getInstance()
+        val timeOfDay = calendar.get(Calendar.HOUR_OF_DAY)
+
+        return getString(
+            when (timeOfDay) {
+                in GOOD_MORNING_RANGE -> R.string.general_text_good_morning
+                in GOOD_MORNING_AFTERNOON -> R.string.general_text_good_afternoon
+                in GOOD_MORNING_AFTERNOON -> R.string.general_text_good_evening
+                else -> R.string.general_text_good_night
+            }
+        )
+    }
+
+    companion object {
+        private val GOOD_MORNING_RANGE = 0..11
+        private val GOOD_MORNING_AFTERNOON = 12..15
+        private val GOOD_MORNING_EVENING = 16..19
     }
 }
