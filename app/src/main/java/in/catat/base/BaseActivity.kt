6da52710b0637faa41d5ee02107c85378791e651
Irfan Pertadima <pertadimairfan@gmail.com>
2020-10-18 00:28:37 +0700

    [irfan] feat: add google sign in method

--- app/src/main/java/in/catat/base/BaseActivity.kt
+++ app/src/main/java/in/catat/base/BaseActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
@@ -15,6 +16,14 @@ import androidx.lifecycle.Observer
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import id.co.catatin.core.commons.CustomViewUnbinder
 import id.co.catatin.core.ext.getColorCompat
 
@@ -24,6 +33,12 @@ import id.co.catatin.core.ext.getColorCompat
  */
 
 abstract class BaseActivity : AppCompatActivity {
+    protected lateinit var auth: FirebaseAuth
+
+    val RC_SIGN_IN: Int = 1
+    protected lateinit var googleSignInClient: GoogleSignInClient
+    protected lateinit var googleSignInOptions: GoogleSignInOptions
+
     @LayoutRes
     private var contentLayoutId: Int = -1
 
@@ -82,13 +97,52 @@ abstract class BaseActivity : AppCompatActivity {
         super.onDestroy()
     }
 
-    fun checkIsUserLoggedIn(userLoginAction: () -> Unit) {
+    protected fun checkIsUserLoggedIn(userLoginAction: () -> Unit) {
         //TODO: CHECK USER LOGGED IN
         val isUserLogin = false
         if (isUserLogin) {
             userLoginAction.invoke()
         } else {
+            //TODO: CHANGE TO NEW ACTIVITY RESULT API
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    protected fun configureGoogleSignIn() {
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+    }
+
+    //TODO: CHANGE TO NEW ACTIVITY RESULT API
+    protected fun signIn() {
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                finish()
+            } else {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
--- app/src/main/java/in/catat/presentation/login/LoginActivity.kt
+++ app/src/main/java/in/catat/presentation/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package `in`.catat.presentation.login
 
 import `in`.catat.R
 import `in`.catat.base.BaseActivity
+import android.app.Activity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 /**
@@ -10,11 +12,20 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity(R.layout.activity_login) {
     override fun onViewCreated() {
+        auth = FirebaseAuth.getInstance()
         setupToolbar()
+        doGoogleLogin()
+        configureGoogleSignIn()
     }
 
     override fun onViewModelObserver() {
 
+    }
+
+    private fun doGoogleLogin() {
+        login_button_google.setOnClickListener {
+            signIn()
+        }
     }
 
     private fun setupToolbar() {
